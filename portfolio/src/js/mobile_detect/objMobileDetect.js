f0e9c021ab50d905217953c5ab80d1ea82d0a91b
Alexey <laiten808@gmail.com>
2017-12-28 16:01:31 +0400

    -

--- portfolio/src/js/mobile_detect/objMobileDetect.js
+++ portfolio/src/js/mobile_detect/objMobileDetect.js
@@ -9,27 +9,6 @@ md.setStyle = function (x) {
     document.getElementsByTagName("head")[0].appendChild(a);
 }
 
-// console.log( md.mobile() );          // 'Sony'
-// console.log( md.phone() );           // 'Sony'
-// console.log( md.tablet() );          // null
-// console.log( md.userAgent() );       // 'Safari'
-// console.log( md.os() );              // 'AndroidOS'
-// console.log( md.is('iPhone') );      // false
-// console.log( md.is('bot') );         // false
-// console.log( md.version('Webkit') );         // 534.3
-// console.log( md.versionStr('Build') );       // '4.1.A.0.562'
-// console.log( md.match('playstation|xbox') ); // false
-
-// console.log( md.userAgent() ); 
-// console.log( md.phone() );
-// console.log( md.tablet() ); 
-// console.log( md.mobileGrade() );
-
-
-
-// location.href = (md.mobileGrade() === 'A') ? '/mobile/' : '/lynx/';
-
-
 if (md.mobile() && md.phone()) {
     console.log('run setStyle for mobile');
     md.setStyle('css/m-main.css');
